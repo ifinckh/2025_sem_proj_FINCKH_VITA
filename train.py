93445148f39e1c73c77ad9ab77565ee715ece0e0
Mathis Finckh <mathis.finckh@epfl.ch>
2025-12-28 19:41:28 +0100

    Started 3dr training, like the og,. for longer

--- train.py
+++ train.py
@@ -99,20 +99,20 @@ def fetch_optimizer(args, model):
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(85))
     elif args.scheduler == 'Reduce':
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.8, min_lr = 0.000005, patience=0,  eps=args.epsilon)
-    # elif args.scheduler == 'OneCycle':
-    #     scheduler = optim.lr_scheduler.OneCycleLR(optimizer, args.lr, args.num_steps+100,
-    #         pct_start=0.05, cycle_momentum=False, anneal_strategy='linear')
-    elif args.scheduler == 'OneCycle': # temp
-        scheduler = optim.lr_scheduler.OneCycleLR(
-            optimizer, 
-            max_lr=args.lr, 
-            total_steps=args.num_steps + 10,  # Match total_steps strictly
-            pct_start=0.1,                    # Spend a bit more time warming up (5% -> 10%)
-            cycle_momentum=False,             # Keep as False for AdamW
-            anneal_strategy='cos',            # CHANGE THIS: linear -> cos
-            div_factor=25.0,                  # Default, but good to be explicit
-            final_div_factor=1000.0           # Forces final LR to be very tiny (3.5e-7)
-        )
+    elif args.scheduler == 'OneCycle':
+        scheduler = optim.lr_scheduler.OneCycleLR(optimizer, args.lr, args.num_steps+100,
+            pct_start=0.05, cycle_momentum=False, anneal_strategy='linear')
+    # elif args.scheduler == 'OneCycle': # temp
+    #     scheduler = optim.lr_scheduler.OneCycleLR(
+    #         optimizer, 
+    #         max_lr=args.lr, 
+    #         total_steps=args.num_steps + 10,  # Match total_steps strictly
+    #         pct_start=0.1,                    # Spend a bit more time warming up (5% -> 10%)
+    #         cycle_momentum=False,             # Keep as False for AdamW
+    #         anneal_strategy='cos',            # CHANGE THIS: linear -> cos
+    #         div_factor=25.0,                  # Default, but good to be explicit
+    #         final_div_factor=1000.0           # Forces final LR to be very tiny (3.5e-7)
+    #     )
     elif args.scheduler == 'MultiCycle':
         scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.000005, max_lr=args.lr, # 9000  171000
             step_size_up=9000, step_size_down=171000, mode='triangular2',cycle_momentum = False)  #mode in ['triangular', 'triangular2', 'exp_range']
